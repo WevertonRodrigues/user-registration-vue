65cf94ba28d0db8e9d3e39a6a1422dda04da8b8d
Weverton Rodrigues <weverton.rodrigues96@yahoo.com>
2021-11-24 00:29:24 -0300

    fix(nuxt.config.js): Changed favicon config

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,11 +16,33 @@ export default {
       {
         hid: 'description',
         name: 'description',
-        content: 'Mini sistema para cadastro de usuários.',
+        content: 'Mini sistema para registro de usuários.',
       },
       { name: 'format-detection', content: 'telephone=no' },
+      { name: 'msapplication-TileColor', content: '#da532c' },
+      { name: 'theme-color', content: '#ffffff' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
